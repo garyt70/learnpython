df594802a6652d2a97e63a63de6853de644d217b
garyt70 <g4thompson@gmail.com>
2017-02-25 21:38:13 +0000

    next tutorial using - using variables

--- ex1.py
+++ ex1.py
@@ -7,3 +7,16 @@ print ("Test math", 25+30/6)
 print ("Roosters", 100-25*3%4)
 print ("Now a bigger formula")
 print (3+2+1-5+4%2-1/4+6)
+
+#tutorial working with variables
+cars = 100
+space_in_a_car=4.0
+drivers=30
+passengers=90
+carsNotDriven= cars - drivers
+carsDriven = drivers
+carpoolCapacity = carsDriven * space_in_a_car
+averagePassangersPerCar = passengers/carsDriven
+
+print ("There are", cars, "cars available.")
+print ("There are only", drivers,"drivers available.")
