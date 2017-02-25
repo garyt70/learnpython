9ceccb64f6a22ff1126d93e81dd0207f1f10f0e3
garyt70 <g4thompson@gmail.com>
2017-02-25 21:27:16 +0000

    tutorial 2

--- ex1.py
+++ ex1.py
@@ -2,3 +2,8 @@ print( "Hello World!")
 print ("Hello Again")
 print ("I like type this.")
 print ("Yay!")
+
+print ("Test math", 25+30/6)
+print ("Roosters", 100-25*3%4)
+print ("Now a bigger formula")
+print (3+2+1-5+4%2-1/4+6)
