9a6bd0385a0995b9e479ff36ef1824fe250831e0
garyt70 <g4thompson@gmail.com>
2017-02-26 21:34:39 +0000

    tutorial - more file writing

--- ex1.py
+++ ex1.py
@@ -31,5 +31,11 @@ print ("your age is %r" % age)
 with open("test_doc.txt","r+") as file1:
     print ("====  \n\n\n\n")
     print ( file1.read() )
+    file1.write("54321\n")
+    file1.write("end of file\n")
+    file1.seek(0)
+    for line in file1:
+        print( line )
+        print("---\n")
 
 print ("Files is closed, correct?", file1.closed)
